027c618a6e51bd9f9d8238ef78d73fcee1da04a5
Arpeet-gupta <agarpitgupta96@gmail.com>
2021-01-11 04:51:27 +0000

    Enhancement v3.1.0:  Adding functionality "Serving Files Using http.FileServer() API"

--- go-webserver.go
+++ go-webserver.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 
+	//create fileserver handler
+	fs := http.FileServer(http.Dir("/home/vagrant/main"))
 	// handle `/` route to `http.DefaultServeMux`
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "Hello World!")
+		res.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(res, "<h1>Golang!</h1>")
 	})
-	// handle `/hello/golang` route to `http.DefaultServeMux`
-	http.HandleFunc("/hello/golang", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "Hello Golang!")
-	})
-	//listen and Serve using `ServeMux`
-	http.ListenAndServe(":9090", nil)
+	// handle `/static` route
+	http.Handle("/static/", http.StripPrefix("/static", fs))
+
+	// start HTTP server with `http.DefaultServeMux` handler
+	log.Fatal(http.ListenAndServe(":9090", nil))
 }
